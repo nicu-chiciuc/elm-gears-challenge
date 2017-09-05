132f36aefbc7615cb93524e28216a785e3100971
nicu-chiciuc <nicuchiciuc@gmail.com>
2017-09-05 21:37:41 +0300

    Circles are draggable.

--- Main.elm
+++ Main.elm
@@ -4,6 +4,7 @@ import Basics exposing (pi)
 import Html
 import Json.Decode as Json
 import Svg exposing (Svg)
+import Svg.Events exposing (onMouseDown, onMouseUp)
 import Svg.Attributes exposing (..)
 import Task
 import VirtualDom
@@ -14,9 +15,16 @@ type alias Position =
     { x : Int, y : Int }
 
 
+type alias CircleData =
+    ( Float, Float, Float, Int )
+
+
 type alias Model =
     { size : Window.Size
     , pos : Position
+    , mouseDown : Bool
+    , circleData : List CircleData
+    , currentCircleInd : Maybe Int
     }
 
 
@@ -24,6 +32,8 @@ type Msg
     = Error
     | WindowSize Window.Size
     | MouseMove Position
+    | MouseDown Int
+    | MouseUp
 
 
 marginScene =
@@ -44,6 +54,14 @@ init : ( Model, Cmd Msg )
 init =
     ( { size = Window.Size 600 600
       , pos = Position 0 0
+      , mouseDown = False
+      , circleData =
+            [ ( 0, 0, 16, 0 )
+            , ( -160, 0, 12, 1 )
+            , ( -112, 188, 24, 2 )
+            , ( -50, 188, 20, 3 )
+            ]
+      , currentCircleInd = Nothing
       }
     , Task.perform WindowSize Window.size
     )
@@ -56,10 +74,45 @@ update msg model =
             ( { model | size = Window.Size (width - 2 * marginScene) (height - 100 - 2 * marginScene) }, Cmd.none )
 
         MouseMove pos ->
-            ( { model | pos = pos }, Cmd.none )
+            if model.mouseDown then
+                case model.currentCircleInd of
+                    Nothing ->
+                        ( { model | pos = pos }, Cmd.none )
+
+                    Just ind ->
+                        let
+                            newCircleData =
+                                updateCircleData model.circleData model.pos ind
+                        in
+                            ( { model | pos = pos, circleData = newCircleData }, Cmd.none )
+            else
+                ( model, Cmd.none )
+
+        MouseDown ind ->
+            ( { model
+                | mouseDown = True
+                , currentCircleInd = Just ind
+              }
+            , Cmd.none
+            )
+
+        MouseUp ->
+            ( { model | mouseDown = False }, Cmd.none )
 
         _ ->
             Debug.crash "update"
+
+
+updateCircleData : List CircleData -> Position -> Int -> List CircleData
+updateCircleData list { x, y } ind =
+    let
+        toggle index ( ox, oy, rad, i ) =
+            if i == ind then
+                ( toFloat x - dispx, toFloat y - dispy, rad, i )
+            else
+                ( ox, oy, rad, i )
+    in
+        List.indexedMap toggle list
 
 
 view : Model -> Html.Html Msg
@@ -70,15 +123,26 @@ view model =
         ]
 
 
+dispx : Float
+dispx =
+    200
+
+
+dispy : Float
+dispy =
+    100
+
+
 scene : Model -> Html.Html Msg
 scene model =
     Svg.svg
         [ width <| toString model.size.width
         , height <| toString model.size.height
         , style ("margin-left:" ++ px marginScene)
+        , VirtualDom.onWithOptions "mousemove" options (Json.map MouseMove offsetPosition)
         ]
         [ background model
-        , grouping [ circle model, sprok model ]
+        , grouping [ sproks model ]
         ]
 
 
@@ -93,7 +157,6 @@ background model =
         [ width <| toString <| model.size.width - 20
         , height <| toString <| model.size.height - 20
         , fill "gray"
-        , VirtualDom.onWithOptions "mousemove" options (Json.map MouseMove offsetPosition)
         ]
         []
 
@@ -133,23 +196,33 @@ circle model =
 
 
 grouping : List (Svg Msg) -> Svg Msg
-grouping msgs =
+grouping =
     Svg.g
         []
-        msgs
 
 
-sprok : Model -> Svg Msg
-sprok model =
+sproks : Model -> Svg Msg
+sproks model =
+    Svg.g []
+        (List.map
+            sprok
+            model.circleData
+        )
+
+
+sprok : CircleData -> Svg Msg
+sprok ( x, y, rad, ind ) =
     Svg.circle
-        [ fill "none"
-        , r "30"
-        , cx (toString model.pos.x)
-        , cy (toString model.pos.y)
+        [ fill "blue"
+        , r (toString rad)
+        , cx (dispx + x |> toString)
+        , cy (dispy + y |> toString)
         , strokeDasharray (toString (pi * 2))
         , strokeDashoffset "1"
         , stroke "#ff0"
         , strokeWidth "4"
+        , onMouseDown (MouseDown ind)
+        , onMouseUp MouseUp
         ]
         [ Svg.animate
             [ attributeName "stroke-dashoffset"
